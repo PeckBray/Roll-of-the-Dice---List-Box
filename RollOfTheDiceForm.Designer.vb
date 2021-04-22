e6f38979081e30958d1e30a8dbad192406914327
Brayden Peck <brayden.peck2@gmail.com>
2021-04-21 18:53:23 -0600

    Add project files.

---
+++ RollOfTheDiceForm.Designer.vb
@@ -0,0 +1,88 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class RollOfTheDiceForm
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.ListBox1 = New System.Windows.Forms.ListBox()
+        Me.RollButton = New System.Windows.Forms.Button()
+        Me.ClearButton = New System.Windows.Forms.Button()
+        Me.ExitButton = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'ListBox1
+        '
+        Me.ListBox1.FormattingEnabled = True
+        Me.ListBox1.Location = New System.Drawing.Point(144, 59)
+        Me.ListBox1.Name = "ListBox1"
+        Me.ListBox1.Size = New System.Drawing.Size(506, 173)
+        Me.ListBox1.TabIndex = 0
+        '
+        'RollButton
+        '
+        Me.RollButton.Location = New System.Drawing.Point(341, 341)
+        Me.RollButton.Name = "RollButton"
+        Me.RollButton.Size = New System.Drawing.Size(97, 58)
+        Me.RollButton.TabIndex = 1
+        Me.RollButton.Text = "&Roll"
+        Me.RollButton.UseVisualStyleBackColor = True
+        '
+        'ClearButton
+        '
+        Me.ClearButton.Location = New System.Drawing.Point(481, 341)
+        Me.ClearButton.Name = "ClearButton"
+        Me.ClearButton.Size = New System.Drawing.Size(99, 58)
+        Me.ClearButton.TabIndex = 2
+        Me.ClearButton.Text = "&Clear"
+        Me.ClearButton.UseVisualStyleBackColor = True
+        '
+        'ExitButton
+        '
+        Me.ExitButton.DialogResult = System.Windows.Forms.DialogResult.Cancel
+        Me.ExitButton.Location = New System.Drawing.Point(630, 341)
+        Me.ExitButton.Name = "ExitButton"
+        Me.ExitButton.Size = New System.Drawing.Size(92, 58)
+        Me.ExitButton.TabIndex = 3
+        Me.ExitButton.Text = "&Exit"
+        Me.ExitButton.UseVisualStyleBackColor = True
+        '
+        'RollOfTheDiceForm
+        '
+        Me.AcceptButton = Me.RollButton
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.CancelButton = Me.ExitButton
+        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.ExitButton)
+        Me.Controls.Add(Me.ClearButton)
+        Me.Controls.Add(Me.RollButton)
+        Me.Controls.Add(Me.ListBox1)
+        Me.Name = "RollOfTheDiceForm"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents ListBox1 As ListBox
+    Friend WithEvents RollButton As Button
+    Friend WithEvents ClearButton As Button
+    Friend WithEvents ExitButton As Button
+End Class
---
+++ RollOfTheDiceForm.vb
@@ -0,0 +1,74 @@
+﻿Public Class RollOfTheDiceForm
+    Private Sub ExitButton_Click(sender As Object, e As EventArgs) Handles ExitButton.Click
+        Me.Close()
+    End Sub
+
+    Private Sub ClearButton_Click(sender As Object, e As EventArgs) Handles ClearButton.Click
+        ListBox1.Items.Clear()
+        RollDice()
+    End Sub
+
+    Private Sub RollButton_Click(sender As Object, e As EventArgs) Handles RollButton.Click
+        RollDice()
+    End Sub
+
+    Sub RollDice()
+        Static randomNumbers(10) As Integer
+        Dim numbers(0, 10) As String
+        Dim rollsArray(0, 10) As String
+        Dim rollNumbers(0, 10) As Integer
+        Dim arrayNumbers As String
+        Dim rolls As String
+        ListBox1.Items.Clear()
+        If RollButton.Focused = True Then
+            '"roll" for random numbers between 0 and 10
+            For i = 0 To 1000
+                randomNumbers(GetRandomNumber(10)) += 1
+
+            Next
+
+            ListBox1.Items.Add("Roll of the Dice")
+
+            'create divider between the lines
+            ListBox1.Items.Add(StrDup(100, "-"))
+
+            'list the numbers from 2 to 12 and add dividers between numbers
+            For i = 2 To 12
+                numbers(0, i - 2) = (Str(i).PadLeft(7) & "|")
+            Next
+            For i = 0 To 10
+                arrayNumbers = $"{arrayNumbers}{numbers(0, i)}"
+            Next
+            ListBox1.Items.Add(arrayNumbers)
+            'create divider between the lines
+            ListBox1.Items.Add(StrDup(100, "-"))
+
+            'will write out how many times each number was "rolled" and divides the numbers with lines
+            For i = 0 To UBound(randomNumbers)
+                rollsArray(0, i) = (Str(randomNumbers(i)).PadLeft(6) & "|")
+            Next
+            For i = 0 To 10
+                rolls = $"{rolls}{rollsArray(0, i)}"
+            Next
+            ListBox1.Items.Add(rolls)
+            'create divider between the lines
+            ListBox1.Items.Add(StrDup(100, "-"))
+
+        Else
+            'this code allows for the data of previous rolls to be stored while allowing the data to be cleared.
+            For i = 0 To 10
+                randomNumbers(i) = 0
+            Next
+        End If
+    End Sub
+
+    ''' <summary>
+    ''' this function will return a random number between 1 and a maximum number that is input
+    ''' </summary>
+    ''' <param name="maxNumber"></param>
+    ''' <returns></returns>
+    Function GetRandomNumber(maxNumber As Integer) As Integer
+        Randomize(DateTime.Now.Millisecond)
+        Return CInt(Math.Floor(Rnd() * (maxNumber + 1)))
+    End Function
+End Class
